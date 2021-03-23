b10039d6a570ccf199f9a70c2b563c2219ae8343
Royer G <royjuni3431@gmail.com>
2021-03-23 17:50:41 -0500

    feat: add products models and create a basic view to cart

--- products/models.py
+++ products/models.py
@@ -14,3 +14,37 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    discount = models.FloatField(default=0)
+    video = models.URLField(null=True, blank=True)
+
+
+class ProductVariation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    color = models.CharField(max_length=7, blank=True, null=True, help_text='Hexadecimal colors only')
+    height = models.FloatField(blank=True, null=True, help_text='Please add measurement in centimeters.')
+    width = models.FloatField(blank=True, null=True, help_text='Please add measurement in centimeters.')
+    length = models.FloatField(blank=True, null=True, help_text='Please add measurement in centimeters.')
+    weight = models.FloatField(blank=True, null=True, help_text='Please add measurement in centimeters.')
+    material = models.CharField(max_length=30, blank=True, null=True)
+
+    SIZE_CHOICES = [
+        ('XS', 'XS: Extra Small'),
+        ('S', 'S: Small'),
+        ('M', 'M: Medium'),
+        ('L', 'L: Large'),
+        ('XL', 'XL: Extra Large'),
+        ('XXL', 'XXL: Extra Extra Large'),
+    ]
+
+    size = models.FloatField(blank=True, null=True, choices=SIZE_CHOICES)
+
+
+class ProductPicture(models.Model):
+    product_variation = models.ForeignKey(ProductVariation, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='products/')
